a95df6b25c0515e696b28f7fd785545874d6f39d
ivaylo91 <ipenev91@gmail.com>
2020-12-12 21:31:07 +0200

    Site Finished

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function myFunc() {
+    let menu = document.getElementById('site-navigation');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
